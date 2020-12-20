6cc544e867cf8d747ad9e0681abb6e0cc6b6af65
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-20 22:49:01 +0530

    initial commit

---
+++ monitor-app/src/main/java/com/monitor/monitorapp/MonitorAppApplication.java
@@ -0,0 +1,13 @@
+package com.monitor.monitorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorAppApplication.class, args);
+	}
+
+}
