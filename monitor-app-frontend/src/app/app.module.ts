31c79ec6dc4e1b8f225980c37ac51c62c20d7a2e
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-22 00:34:44 +0530

    profile component

--- monitor-app-frontend/src/app/app.module.ts
+++ monitor-app-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
