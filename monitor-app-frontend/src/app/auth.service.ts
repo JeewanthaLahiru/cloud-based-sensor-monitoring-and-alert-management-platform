ea760401f5b3656772a29dba3aa251742824995c
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-22 14:39:10 +0530

    auth service user registration method

--- monitor-app-frontend/src/app/auth.service.ts
+++ monitor-app-frontend/src/app/auth.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UserRegisterModel } from './UserRegisterModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private _registerUrl = "http://localhost:3000/api/register";
+
   constructor(private _http:HttpClient) { }
+
+  registerUser(user:UserRegisterModel){
+    return this._http.post<any>(this._registerUrl,user);
+  }
+
 }
