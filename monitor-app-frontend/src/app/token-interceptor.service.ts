3464ef723c66fbcb810caf8426b9aca0aebd0ed6
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-23 20:24:37 +0530

    token interceptor service

--- monitor-app-frontend/src/app/token-interceptor.service.ts
+++ monitor-app-frontend/src/app/token-interceptor.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpInterceptor } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService {
+export class TokenInterceptorService implements HttpInterceptor {
 
   constructor() { }
+
+  intercept(req,next){
+    let tokenizedReq = req.clone({
+      setHeaders:{
+        Authorization:'Bearer xx.yy.zz'
+      }
+    })
+    return next.handle(tokenizedReq)
+  }
+
 }
