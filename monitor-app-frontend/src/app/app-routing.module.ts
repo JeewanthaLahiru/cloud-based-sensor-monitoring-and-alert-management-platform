042a29968ba4169eca852b458802d64d408c2714
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-23 20:09:56 +0530

    addeth auth guard to app routing module

--- monitor-app-frontend/src/app/app-routing.module.ts
+++ monitor-app-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -10,7 +11,7 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
-  {path:'profile',component:ProfileComponent},
+  {path:'profile',component:ProfileComponent,canActivate:[AuthGuard]},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
   {path:'**',component:NotFoundComponent}
