a3ca0bec97c11c54a0a9ab47cf8b6018a8312841
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-24 02:12:27 +0530

    chart changes

--- monitor-app-frontend/src/app/profile/profile.component.ts
+++ monitor-app-frontend/src/app/profile/profile.component.ts
@@ -11,7 +11,8 @@ import { Chart } from 'node_modules/chart.js';
 })
 export class ProfileComponent implements OnInit {
 
-  weather: IWeather[]
+  weather: IWeather[];
+  
 
   _token = localStorage.getItem('token')
 
@@ -22,43 +23,6 @@ export class ProfileComponent implements OnInit {
       this.weather = data
     })
 
-
-    var myChart = new Chart("myChart", {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
   }
 
   getDecodedAccessToken(token: string): any {
