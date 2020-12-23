f0c2bf3e18fda462f74e9f67d0a2da24315c4612
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-23 20:07:19 +0530

    auth guard routing method

--- monitor-app-frontend/src/app/auth.guard.ts
+++ monitor-app-frontend/src/app/auth.guard.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private _authService:AuthService,
+    private _router:Router){}
+
+  canActivate():boolean{
+    if(this._authService.loggedIn()){
+      return true;
+    }else{
+      this._router.navigate(['/login']);
+      return false;
+    }
   }
   
 }
