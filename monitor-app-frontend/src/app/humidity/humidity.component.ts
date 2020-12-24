81cba2dc339df29a5dc2d6b32088e2d0efd29603
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-24 23:26:51 +0530

    humidity and pressure component

---
+++ monitor-app-frontend/src/app/humidity/humidity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-humidity',
+  templateUrl: './humidity.component.html',
+  styleUrls: ['./humidity.component.css']
+})
+export class HumidityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ monitor-app-frontend/src/app/pressure/pressure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pressure',
+  templateUrl: './pressure.component.html',
+  styleUrls: ['./pressure.component.css']
+})
+export class PressureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
