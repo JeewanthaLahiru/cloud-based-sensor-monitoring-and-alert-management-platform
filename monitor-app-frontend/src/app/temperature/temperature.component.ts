4be7e809762212ff391e9d91cbe88f91f900cb17
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-25 00:31:31 +0530

    weather chart to weather component

--- monitor-app-frontend/src/app/temperature/temperature.component.ts
+++ monitor-app-frontend/src/app/temperature/temperature.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IWeather } from '../models/iweather';
+import { WeatherService } from '../weather.service';
+import { Chart } from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-temperature',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemperatureComponent implements OnInit {
 
-  constructor() { }
+  weather:IWeather[]
+
+  constructor(private _weather:WeatherService) { }
 
   ngOnInit(): void {
+    this._weather.getWeather().subscribe(data=>{
+      this.weather = data
+      let weatherDate = []
+      let weatherValue = []
+      this.weather.forEach(function(item){
+        weatherDate.push(Number(item.date.slice(-8,-3)))
+        weatherValue.push(item.data_value.slice(0,2))
+      })
+
+      var myChart = new Chart('myChart', {
+        type: 'line',
+        data: {
+            labels: weatherDate,
+            datasets: [{
+                label: 'weather',
+                data: weatherValue,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            }
+        }
+    });
+
+    })
   }
 
 }
