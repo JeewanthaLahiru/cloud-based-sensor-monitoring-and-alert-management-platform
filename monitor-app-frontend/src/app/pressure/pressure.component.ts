1a96971f1690da2a000483c00b3caf2f6ac6dbb6
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-25 01:15:11 +0530

    pressure chart

--- monitor-app-frontend/src/app/pressure/pressure.component.ts
+++ monitor-app-frontend/src/app/pressure/pressure.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-pressure',
@@ -10,6 +11,31 @@ export class PressureComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var myChart = new Chart('myChart', {
+      type: 'line',
+      data: {
+          labels: ['label1','label2','label3','label4','label5','label1','label2','label3','label4','label5','label1','label2','label3','label4','label5'],
+          datasets: [{
+              label: 'Temperature',
+              data: [65,55,65,70,71,72,70,65,66,63,65,55,65,75,71],
+              backgroundColor: 'rgba(255, 15, 15, 0.2)',
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: false
+                  },
+                  scaleLabel:{
+                    display:true,
+                    labelString:'Pressure'
+                  }
+              }]
+          }
+      }
+  });
   }
 
 }
