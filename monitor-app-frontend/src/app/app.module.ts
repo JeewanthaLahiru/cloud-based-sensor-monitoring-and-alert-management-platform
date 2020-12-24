dab4b8a47690dd692a32b01b1fb0df44cd49c687
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-24 23:18:00 +0530

    temperature component

--- monitor-app-frontend/src/app/app.module.ts
+++ monitor-app-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthGuard } from './auth.guard';
+import { TemperatureComponent } from './temperature/temperature.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { AuthGuard } from './auth.guard';
     RegisterComponent,
     ProfileComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    TemperatureComponent
   ],
   imports: [
     BrowserModule,
