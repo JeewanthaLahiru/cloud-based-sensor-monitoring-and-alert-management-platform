332d5cb3b055669cbdd48542b6d63bc531557c09
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-25 19:10:50 +0530

    added notification model class

--- monitorapp/src/main/java/com/monitor/database/MongoListener.java
+++ monitorapp/src/main/java/com/monitor/database/MongoListener.java
@@ -5,12 +5,10 @@ import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MongoListener extends AbstractMongoEventListener<Integer>
-{
+public class MongoListener extends AbstractMongoEventListener<Integer> {
 
     @Override
     public void onAfterSave(AfterSaveEvent<Integer> event) {
-
         System.out.println("Done");
     }
 
---
+++ monitorapp/src/main/java/com/monitor/database/model/Notification.java
@@ -0,0 +1,26 @@
+package com.monitor.database.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    private String time;
+
+    private String temp;
+
+
+
+}
