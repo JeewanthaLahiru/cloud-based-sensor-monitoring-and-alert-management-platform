cf9590a91d17fbf72e6c527f945423a40080bff7
Chanaka-Madushan-Herath <chanaka.herath.1998@gmail.com>
2020-12-25 18:34:05 +0530

    user register and user delete complete

---
+++ monitorapp/src/main/java/com/monitor/monitorapp/DbSeeder.java
@@ -0,0 +1,42 @@
+package com.monitor.monitorapp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DbSeeder /*implements CommandLineRunner*/ {
+    private UserRepository userRepository;
+
+    public DbSeeder(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /*@Override
+    public void run(String... strings) throws Exception {
+        User Chanaka = new User(
+                "Chanaka",
+                "chanaka@gmail.com",
+                "cmax12",
+                "0767025709",
+                "Email"
+        );
+
+        User Jeewantha = new User(
+                "jeewantha",
+                "jeewantha@gmail.com",
+                "jeewa12",
+                "074583825709",
+                "Text"
+        );
+
+        this.userRepository.deleteAll();
+
+
+        List<User> users = Arrays.asList(Chanaka, Jeewantha);
+        this.userRepository.saveAll(users);
+    }*/
+}
