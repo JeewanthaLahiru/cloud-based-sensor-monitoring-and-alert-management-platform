0103c3c32a240e7ef1e698686f4ae1e0d1b58718
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-25 23:02:37 +0530

    implemented TempDataController

--- monitorapp/src/main/java/com/monitor/database/web/controller/TempDataController.java
+++ monitorapp/src/main/java/com/monitor/database/web/controller/TempDataController.java
@@ -1,4 +1,25 @@
 package com.monitor.database.web.controller;
 
+import com.monitor.database.model.TempData;
+import com.monitor.database.repository.TempDataRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/data")
 public class TempDataController {
+
+    private TempDataRepository tempDataRepository;
+
+    public TempDataController(TempDataRepository tempDataRepository) {
+        this.tempDataRepository = tempDataRepository;
+    }
+
+    @GetMapping("/all")
+    public List<TempData> getAll() {
+        return this.tempDataRepository.findAll();
+    }
 }
