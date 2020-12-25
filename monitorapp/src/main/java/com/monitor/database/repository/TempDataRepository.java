d69eef7daf80401d310356fbfe1246c64e3cf2d9
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-25 22:43:31 +0530

    implemented TempDataRepository interface

---
+++ monitorapp/src/main/java/com/monitor/database/repository/TempDataRepository.java
@@ -0,0 +1,9 @@
+package com.monitor.database.repository;
+
+import com.monitor.database.model.TempData;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TempDataRepository extends MongoRepository<TempData, String> {
+}
