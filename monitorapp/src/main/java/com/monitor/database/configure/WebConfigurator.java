550f73725e1aeb4b64b6c2cbcbf48de4dc66bffb
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-28 01:11:29 +0530

    refractored WebConfigurator

--- monitorapp/src/main/java/com/monitor/database/configure/WebConfigurator.java
+++ monitorapp/src/main/java/com/monitor/database/configure/WebConfigurator.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfigurator extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- monitorapp/src/main/java/com/monitor/database/listen/TempDataRepositoryListener.java
+++ monitorapp/src/main/java/com/monitor/database/listen/TempDataRepositoryListener.java
@@ -1,4 +1,14 @@
 package com.monitor.database.listen;
 
-public class TempDataRepositoryListener {
+import com.monitor.database.model.TempData;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
+
+public class TempDataRepositoryListener extends AbstractMongoEventListener<TempData> {
+
+    @Override
+    public void onBeforeSave(BeforeSaveEvent<TempData> event) {
+        super.onBeforeSave(event);
+        System.out.println("Saved");
+    }
 }
