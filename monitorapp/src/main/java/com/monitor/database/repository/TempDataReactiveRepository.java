9ed80e9349a92508b813a4d07d94c0f2c4503cff
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-28 02:56:28 +0530

    implemented TempDataReactiveRepository

--- monitorapp/src/main/java/com/monitor/database/repository/TempDataReactiveRepository.java
+++ monitorapp/src/main/java/com/monitor/database/repository/TempDataReactiveRepository.java
@@ -1,4 +1,10 @@
 package com.monitor.database.repository;
 
-public class TempDataReactiveRepository {
+import com.monitor.database.model.TempData;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface TempDataReactiveRepository extends ReactiveMongoRepository<TempData, String> {
+
 }
