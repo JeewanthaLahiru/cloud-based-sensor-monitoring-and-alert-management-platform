96d0d22bb632fa368d6813aeaa2568e5b0250bb0
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-28 03:01:07 +0530

    implemented TempDataReactiveController stage0

--- monitorapp/src/main/java/com/monitor/database/web/controller/TempDataReactiveController.java
+++ monitorapp/src/main/java/com/monitor/database/web/controller/TempDataReactiveController.java
@@ -1,4 +1,30 @@
 package com.monitor.database.web.controller;
 
+
+import com.monitor.database.model.TempData;
+import com.monitor.database.repository.TempDataReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/tempdata/live")
 public class TempDataReactiveController {
+
+    private TempDataReactiveRepository tempDataReactiveRepository;
+
+    @Autowired
+    public TempDataReactiveController(TempDataReactiveRepository tempDataReactiveRepository) {
+        this.tempDataReactiveRepository = tempDataReactiveRepository;
+    }
+
+    @GetMapping("/all")
+    public Flux<TempData> getByTailing() {
+        Flux<TempData> tempDataFlux = this.tempDataReactiveRepository.findAll();
+        return tempDataFlux;
+    }
+
+
 }
