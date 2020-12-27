c5a8ee2de53592a3f70f6d39dddecbb0c8aac45f
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-28 02:26:12 +0530

    implemented notification adding part stage:2:saving completed

--- monitorapp/src/main/java/com/monitor/database/listen/TempDataRepositoryListener.java
+++ monitorapp/src/main/java/com/monitor/database/listen/TempDataRepositoryListener.java
@@ -23,7 +23,7 @@ public class TempDataRepositoryListener extends AbstractMongoEventListener<TempD
         super.onBeforeSave(event);
         TempData tempData = event.getSource();
         if (Integer.parseInt(tempData.getData_value().substring(0,2)) > DatabaseListenerConfigurator.TEMP_THRESHOLD) {
-            this.notificationReactiveRepository.save(new Notification(tempData.getDate(),tempData.getData_value()));
+            this.notificationReactiveRepository.save(new Notification(tempData.getDate(), tempData.getData_value())).subscribe();
             System.out.println("Saved");
         }
     }
