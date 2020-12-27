184059e4337762a7d991692b5d0c278d3960cfcf
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-28 01:45:54 +0530

    implemented getter and setter for TempData date

--- monitorapp/src/main/java/com/monitor/database/model/TempData.java
+++ monitorapp/src/main/java/com/monitor/database/model/TempData.java
@@ -20,4 +20,12 @@ public class TempData {
     private String sensor_id;
     private String date;
     private String data_value;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
