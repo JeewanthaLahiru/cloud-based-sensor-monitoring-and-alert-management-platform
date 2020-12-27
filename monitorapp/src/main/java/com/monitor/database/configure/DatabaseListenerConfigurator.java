33ca2cd740c00e553f0ce6230a419672c8bf4f90
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-28 01:14:11 +0530

    implemented DatabaseListenerConfigurator

--- monitorapp/src/main/java/com/monitor/database/configure/DatabaseListenerConfigurator.java
+++ monitorapp/src/main/java/com/monitor/database/configure/DatabaseListenerConfigurator.java
@@ -1,4 +1,15 @@
 package com.monitor.database.configure;
 
+import com.monitor.database.listen.TempDataRepositoryListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DatabaseListenerConfigurator {
+
+    @Bean
+    public TempDataRepositoryListener tempDataRepositoryListener() {
+        return new TempDataRepositoryListener();
+    }
+
 }
