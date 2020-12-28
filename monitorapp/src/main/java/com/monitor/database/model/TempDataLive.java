fa86772db5e1c17d7b726e11d698565faddf33a1
Neminda Prabhashwara <nemindaprabhashwara@gmail.com>
2020-12-28 12:04:39 +0530

    implemented TempDataLive to support live data coverage

--- monitorapp/src/main/java/com/monitor/database/model/TempDataLive.java
+++ monitorapp/src/main/java/com/monitor/database/model/TempDataLive.java
@@ -1,4 +1,39 @@
 package com.monitor.database.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "temp_data_live")
 public class TempDataLive {
+
+    @Id
+    private String id;
+
+    private String sensor_id;
+    private String date;
+    private String data_value;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getData_value() {
+        return data_value;
+    }
+
+    public void setData_value(String data_value) {
+        this.data_value = data_value;
+    }
 }
