79beb96324da114512b6236026f73be505711166
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-12-29 13:36:14 +0530

    backend

---
+++ monitorapp/src/test/java/com/monitor/MonitorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
