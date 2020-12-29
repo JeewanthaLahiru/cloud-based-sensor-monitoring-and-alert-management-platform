921a0423c13f3e504302e84671ffb3adceb0f7d8
Chanaka-Madushan-Herath <chanaka.herath.1998@gmail.com>
2020-12-29 21:16:43 +0530

    Add notification method

--- monitorapp/src/main/java/com/monitor/database/web/controller/NotificationReactiveController.java
+++ monitorapp/src/main/java/com/monitor/database/web/controller/NotificationReactiveController.java
@@ -2,9 +2,13 @@ package com.monitor.database.web.controller;
 
 import com.monitor.database.model.Notification;
 import com.monitor.database.repository.NotificationReactiveRepository;
+import com.monitor.monitorapp.User;
+import com.monitor.monitorapp.UserRepository;
+import com.monitor.monitorapp.utility.JWTUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.Disposable;
@@ -16,6 +20,10 @@ public class NotificationReactiveController {
 
     /*private NotificationService notificationService;*/
     private NotificationReactiveRepository notificationReactiveRepository;
+   @Autowired
+    private  JWTUtility jwtUtility;
+   @Autowired
+   private UserRepository userRepository;
 
     @Autowired
     public NotificationReactiveController(/*NotificationService notificationService, */NotificationReactiveRepository notificationReactiveRepository) {
@@ -29,8 +37,13 @@ public class NotificationReactiveController {
     }*/
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Notification> getByTailing() {
-        System.out.println("fgrhfgd");
+    public Flux<Notification> getByTailing(@RequestHeader("Authorization") String token) {
+
+
+        String username = jwtUtility.getEmailFromToken(token);
+        String email = username.substring(0, username.indexOf(','));
+        User user=this.userRepository.findByEmail(email);
+        String notificationMethod = user.getNotification();
         Flux<Notification> stream = notificationReactiveRepository.findByTime("24");
         Disposable subscription = stream.doOnNext(System.out::println).subscribe();
         return stream;
